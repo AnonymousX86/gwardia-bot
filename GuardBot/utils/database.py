27eed4045014ac77242cd23e640d0c87677d9bd3
Jakub Suchenek <jakub.suchenek.25@gmail.com>
2021-08-28 17:22:52 +0200

    Update "urodziny" command
    Added all basic functions such as: adding, changing, viewing and deleting.

--- GuardBot/utils/database.py
+++ GuardBot/utils/database.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
+from os import path
+from os.path import sep
+from typing import Optional
+
 from sqlalchemy import Column, create_engine, Text, Integer
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-_ENGINE = create_engine('sqlite:///../birthday.db')
+_BASE_DIR = path.dirname(path.abspath(__file__))
+_ENGINE = create_engine(rf'sqlite:///{_BASE_DIR}{2 * sep}..{2 * sep}birthday.db')
+
 _SESSION = sessionmaker()
 _SESSION.configure(bind=_ENGINE)
 BASE = declarative_base()
@@ -14,6 +21,39 @@ class Birthday(BASE):
     user_id = Column(Text(18), primary_key=True)
     date_day = Column(Integer)
     date_month = Column(Integer)
+
+
+def get_birthday(user_id: str) -> Optional[Birthday]:
+    s = _SESSION()
+    try:
+        return s.query(Birthday).filter_by(user_id=user_id).one_or_none()
+    finally:
+        s.close()
+
+
+def set_birthday(user_id: str, day: int, month: int) -> Optional[Birthday]:
+    s = _SESSION()
+    try:
+        new_bday = Birthday(user_id=user_id, date_day=day, date_month=month)
+        s.add(new_bday)
+        s.commit()
+        return new_bday
+    except IntegrityError:
+        s.rollback()
+        return None
+    finally:
+        s.close()
+
+
+def del_birthday(user_id: str):
+    s = _SESSION()
+    try:
+        bday = s.query(Birthday).filter_by(user_id=user_id).one_or_none()
+        s.delete(bday)
+        s.commit()
+        return bool(bday)
+    finally:
+        s.close()
 
 
 def _check_birthday() -> bool:
--- README.md
+++ README.md
@@ -1,6 +1,29 @@
-# Gwardia Bot
+# Guard Bot
 
-## What to do
-- Birthday reminder - only for people with X role.
-  - Role menu.
+Bot społeczności Discord "Gwardia Gamerów".
+
+## Funkcje
+
+Spis wszystkich komend oraz sposobu ich działania
+
+### Urodziny
+
+#### Argumenty:
+- `osoba` (opcjonalne) - wybierz użytkownika, którego ma dotyczyć komenda. Tj. czyje urodziny chcesz sprawdzić lub jako administrator dodać, zmienić, usunąć. Bez podania osoby, domyślnie wybierany jest żądający wywołania komendy.
+
+Po uruchomieniu komendy będą widoczne komunikat potwierdzający oraz otwarte menu.
+
+![Podgląd uruchomionej komendy](https://cdn.discordapp.com/attachments/687040215440949271/881194906180268042/unknown.png)
+
+#### Akcje
+
+1. `Dodaj/zmień` - pozwala dodać lub zmienić swoje urodziny.
+2. `Sprawdź` - pozwala sprawdzić swoje **lub cudze** urodziny.
+3. `Usuń` - pozwala usunąć swoje urodziny.
+
+*Tylko administratorzy mogą wykonywać wszystkie akcje na każdym użytkowniku.*
+
+#### Wybrano
+
+Oznacza użytkownika, którego będą dotyczyć wybrane akcje. **Nie osobę, która wywołała komendę.**
 
